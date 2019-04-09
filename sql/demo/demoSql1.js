c71892356f64e19e5179db77e1ceac0e67b3e6ff
supermao <maobao@jianbing.com>
2019-04-09 20:15:16 +0800

    sql变更

--- sql/demo/demoSql1.js
+++ sql/demo/demoSql1.js
@@ -3,7 +3,8 @@
 * 数据库中存在4条数据
 **/
 // SELECT * FROM city WHERE CountryCode='AFG'
+// 对应的连表查询
 let citys = {
-    cityQuery: `select * from city where CountryCode='AFG'`
+    cityQuery: `select a.country_id, a.country_name, b.city_name from countries a JOIN cities b ON a.country_name = b.city_country;`
 }
 module.exports = citys
